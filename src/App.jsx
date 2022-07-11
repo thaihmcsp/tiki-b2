ec8503ceb967179c20e3e0a3565dc844e277bf3b
DuyDung99 <duydungkoll@gmail.com>
2022-07-11 14:56:05 +0700

    add create-shop

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import CreateShop from './page/user/createShop/createShop'
+
+function App() {
+  return (
+    <div>
+      <CreateShop/>
+    </div>
+  );
+}
+
+export default App;
