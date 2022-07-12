34f19545fad6191c9d52230b03cdc308217b92ef
TienThanh0705 <nguyentienthanh7520.@gmail.com>
2022-07-12 11:17:49 +0700

    sign-up

---
+++ src/page/user/signIn/SignIn.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { ListCountry } from './ListCountry';
+
+import "./style.css"
+
+function SignIn() {
+    return (
+        
+    <div className='box_container'>
+        <div class="menu">
+            <div class="menu-left">
+                <div>
+                <img src="https://salt.tikicdn.com/ts/upload/ae/f5/15/2228f38cf84d1b8451bb49e2c4537081.png" alt='tiki-logo'/>
+                </div>
+            </div>
+
+            <div class="menu-right">
+           
+              
+                <a href="#team">
+                <select defaultValue='Tiếng Việt'
+                    name="abc" id="" className='user-createShop-select'>  
+                               
+                                {ListCountry.map((branch,index) => {
+                                    return (
+                                        <option value={branch} key={index}>{branch}</option>
+                                    )
+                                })}
+                            </select>
+                </a>
+                
+            </div>
+        </div>
+            
+
+            <div className='box'>
+                <div className='box_background'>
+
+                </div>
+                <div className="container">
+                <div className="title"><h1>Đăng nhập</h1></div>
+                        <div className="inputGroup">
+                        <div>
+                            Nhập Email
+                        </div>
+                        <input class="form-control" type="text"  placeholder="Vui lòng nhập email"/>
+                       
+                        <div>
+                            Nhập mật khẩu 
+                        </div>
+                        
+                        <input class="form-control" type="password"  placeholder="Vui lòng nhập mật khẩu"/>
+
+                        </div>
+                    <div>
+                        
+                        <div className="textGroup">
+                        <p> <a href="">Bạn quên mật khẩu</a></p>
+                       
+                            <p> <a href="">Sign in</a></p>
+                        </div>
+                    </div>
+                        <div className="buttonGroup">
+                            <button className="login">Đăng nhập</button>
+                            
+                        </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SignIn;
