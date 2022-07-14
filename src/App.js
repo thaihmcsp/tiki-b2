485b8cc0a4d0ee60123333385e457eee987b012e
Tạ Đức Hiệp <hiepdev96@gmail.com>
2022-07-14 11:49:08 +0700

    user-info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import 'antd/dist/antd.css';
 import './App.css';
+import ProfileList from './page/user/profile/ProfileLeft/ProfileList';
+import ProfileListHeader from './page/user/profile/ProfileLeft/ProfileListHeader';
+import Profile from './page/user/profile/ProfileManu/ProfileMenu';
+import UserInfo from './page/user/profile/UserInfo_data/UserInfo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className= 'userInformation'>
+      <Profile />
+      <div>
+        < ProfileListHeader/>
+      <div className='profile'>
+      <UserInfo />
+      <ProfileList/>
+      </div>
+      </div>
+      </div>
+      
     </div>
   );
 }
---
+++ src/page/user/profile/UserInfo_data/DataMonth.js
@@ -0,0 +1,5 @@
+let DataMonth = ['Tháng'];
+for (let i = 1; i < 13; i++) {
+    DataMonth.push(i);
+}
+export default DataMonth;
