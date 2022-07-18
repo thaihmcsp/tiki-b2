023df51e54bd9a18597dbdfd23014a107f51062b
Pham Bao <phambao@Phams-MacBook-Pro.local>
2022-07-18 15:16:51 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-import 'antd/dist/antd.css'
-import './App.css';
-import Cart from './page/user/cart/Cart';
-
+import "antd/dist/antd.css";
+import "./App.css";
+import Product from "./page/shop/productManagement/Product";
 function App() {
   return (
     <div className="App">
-      <Cart></Cart>
-      
+      <Product />
     </div>
   );
 }
