7eb564a4a7e493901bee802a3737844832484ac7
thanh <trungthanh2388@gmail.com>
2022-07-18 14:27:44 +0700

    shop-dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'antd/dist/antd.css';
 import './App.css';
+import ShopDashBoardAll from './page/shop/dashboard/ShopDashBoardAll';
 import Cart from './page/user/cart/Cart';
 import ProfileList from './page/user/profile/ProfileLeft/ProfileList';
 import ProfileListHeader from './page/user/profile/ProfileLeft/ProfileListHeader';
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className='App'>
       {/* <Cart></Cart> */}
-      <div className= 'userInformation'>
+      {/* <div className= 'userInformation'>
       <Profile />
       <div>
         < ProfileListHeader/>
@@ -19,8 +20,10 @@ function App() {
       <ProfileList/>
       </div>
       </div>
+      </div> */}
+      <div>
+        <ShopDashBoardAll/>
       </div>
-      
     </div>
   );
 }
