cf9c77a127cec23d130a63e123b1fabfc1b35f01
Pham Bao <phambao@Phams-MacBook-Pro.local>
2022-07-25 15:44:45 +0700

    baochimto

--- src/page/shop/productManagement/priceItem/Tablelock.js
+++ src/page/shop/productManagement/priceItem/Tablelock.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
+import { Switch } from "antd";
 
 const PriceTableLock = styled.div`
   border-radius: 8px 8px 0 0;
@@ -59,20 +60,97 @@ const PriceTableLock = styled.div`
           padding: 0;
           border: 1px solid #eee;
         }
+        .next-table-cell {
+          padding: 0;
+          border: 1px solid #eee;
+          .next-table-cell-wrapper {
+            display: inline-flex;
+            align-items: center;
+            height: 56px;
+            padding: 12px 4px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            .special-price {
+              padding: 5px 10px;
+              .btn-special-price {
+                border-radius: 0;
+                padding: 0;
+                height: 20px;
+                line-height: 20px;
+                font-size: 14px;
+                border-width: 0;
+                color: #1a71ff;
+                background-color: transparent;
+                border-color: transparent;
+                cursor: pointer;
+                .btn-special-price:hover {
+                  color: #478af8;
+                  background-color: transparent;
+                  border-color: transparent;
+                  text-decoration: none;
+                }
+              }
+            }
+            .input-field-tableLock {
+              align-items: center;
+              height: 56px;
+              padding: 12px 4px;
+              display: inline-flex;
+              .next-input-tableLock {
+                border-collapse: separate;
+                border-spacing: 0;
+                line-height: 0;
+                display: inline-table;
+                width: 100%;
+                .next-value-tableLock {
+                  font-size: 14px;
+                  border-radius: 8px;
+                  border-right-width: 0;
+                  color: #858b9c;
+                  text-align: center;
+                  border: 1px solid #cbced5;
+                  padding: 0 8px;
+                  display: table-cell;
+                  vertical-align: middle;
+                  white-space: nowrap;
+                  input {
+                    padding-right: 2px;
+                    border-bottom-right-radius: 8px;
+                    height: 34px;
+                    line-height: 34px \0;
+                    padding: 0 8px;
+                    font-size: 14px;
+                    width: 100%;
+                    border: none;
+                    outline: none;
+                    padding: 0;
+                    margin: 0;
+                    font-weight: 400;
+                    vertical-align: middle;
+                    background-color: transparent;
+                    color: #333;
+                  }
+                }
+                .next-value-tableLock-sellKu {
+                  width: 290px !important;
+                }
+              }
+            }
+          }
+        }
       }
     }
     .input-field {
-      margin: 0 8px 8px 0;
-      width: 100%;
-      display: flex;
-
       align-items: center;
+      height: 56px;
+      padding: 12px 4px;
+      display: inline-flex;
       .next-input {
-        width: 120px;
         border-collapse: separate;
         border-spacing: 0;
         line-height: 0;
         display: inline-table;
+        width: 100%;
         .next-input-text {
           font-size: 14px;
           border-radius: 8px;
@@ -104,7 +182,7 @@ const PriceTableLock = styled.div`
             border: 1px solid #cbced5;
             background-color: #fff;
             height: 36px;
-            width: 100%;
+
             border-top-right-radius: 8px;
             border-bottom-right-radius: 8px;
             input {
@@ -133,7 +211,24 @@ const PriceTableLock = styled.div`
     }
   }
 `;
-const Tablelock = () => {
+const Tablelock = ({ data }) => {
+  console.log(215, data);
+  const onChange = (checked) => {
+    console.log(`switch to ${checked}`);
+  };
+
+  useEffect(() => {
+    const tableLockPrice = document.getElementById("tableLock-price");
+    const tableLockSpecial = document.getElementById("tableLock-special");
+    const tableLockWarehouse = document.getElementById("tableLock-warehouse");
+    const tableLockSellKu = document.getElementById("tableLock-sellerKu");
+
+    tableLockPrice.value = data.valuePrice;
+    tableLockSpecial.innerHTML = data.valueSpecial ? data.valueSpecial : "Add";
+    tableLockWarehouse.value = data.valueWarehouse;
+    tableLockSellKu.value = data.valueSellerKu;
+  }, [data]);
+
   return (
     <div>
       <PriceTableLock>
@@ -196,10 +291,46 @@ const Tablelock = () => {
                       <div className="next-input">
                         <span className="next-input-text">VND</span>
                         <span className="next-value">
-                          <input className="next-input-value" type="number" placeholder="Giá" />
+                          <input className="next-input-value" type="number" placeholder="Giá" id="tableLock-price" />
                         </span>
                       </div>
                     </div>
+                  </div>
+                </td>
+                <td className="next-table-cell">
+                  <div className="next-table-cell-wrapper">
+                    <span className="special-price">
+                      <button className="btn-special-price" id="tableLock-special">
+                        Add
+                      </button>
+                    </span>
+                  </div>
+                </td>
+                <td className="next-table-cell">
+                  <div className="next-table-cell-wrapper">
+                    <div className=" input-field-tableLock">
+                      <div className="next-input-tableLock">
+                        <span className="next-value-tableLock">
+                          <input type="number" id="tableLock-warehouse" />
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td className="next-table-cell">
+                  <div className="next-table-cell-wrapper">
+                    <div className=" input-field-tableLock">
+                      <div className="next-input-tableLock">
+                        <span className="next-value-tableLock next-value-tableLock-sellKu">
+                          <input type="number" id="tableLock-sellerKu" />
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <div>
+                    <Switch defaultChecked onChange={onChange} />
                   </div>
                 </td>
               </tr>
