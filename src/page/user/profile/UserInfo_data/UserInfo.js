55c81871d274e110d4bf31a19fa9277e765dbe94
thanh <trungthanh2388@gmail.com>
2022-07-28 14:47:50 +0700

    a

--- src/page/user/profile/UserInfo_data/UserInfo.js
+++ src/page/user/profile/UserInfo_data/UserInfo.js
@@ -6,10 +6,105 @@ import 'antd/dist/antd.css';
 import { Radio } from 'antd';
 import DataMonth from './DataMonth';
 import Countries from './Countries';
+import { Button, Modal } from 'antd';
+import { useState } from 'react';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { message, Upload } from 'antd';
+import './userinfor.css';
 
+const getBase64 = (img, callback) => {
+  const reader = new FileReader();
+  reader.addEventListener('load', () => callback(reader.result));
+  reader.readAsDataURL(img);
+};
+
+const beforeUpload = (file) => {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+
+  if (!isJpgOrPng) {
+    message.error('You can only upload JPG/PNG file!');
+  }
+
+  const isLt2M = file.size / 1024 / 1024 < 2;
+
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!');
+  }
+
+  return isJpgOrPng && isLt2M;
+};
 function UserInfo() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState();
+
+  const handleChange = (info) => {
+    if (info.file.status === 'uploading') {
+      setLoading(true);
+      return;
+    }
+
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
+  };
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </div>
+  );
   return (
-    <div className= {styles.userInfomation}>
+  <div className= {styles.userInfomation}>
+    <>
+      <Modal title="Cập nhật ảnh đại diện" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+      <Upload
+      name="avatar"
+      listType="picture-card"
+      className="avatar-uploader"
+      showUploadList={false}
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      beforeUpload={beforeUpload}
+      onChange={handleChange}
+    >
+      {imageUrl ? (
+        <img
+          src={imageUrl}
+          alt="avatar"
+          style={{
+            width: '300px',
+          }}
+        />
+      ) : (
+        uploadButton
+      )}
+    </Upload>
+      </Modal>
+    </>
       <div className= {styles.acountForm}>
         <div>Thông tin cá nhân</div>
         <div className= {styles.formInfor}>
@@ -22,7 +117,7 @@ function UserInfo() {
               />
             </div>
             <div className={styles.edit}>
-              <img
+              <img onClick={showModal}
                 src='https://frontend.tikicdn.com/_desktop-next/static/img/account/edit.png'
                 class='edit_img'
                 alt=''
