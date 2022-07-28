79e9f9780f3bf8cdc86bd03d94dbba9cd3f9bfeb
DuyDung99 <duydungkoll@gmail.com>
2022-07-28 11:11:28 +0700

    pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css';
-import CreateShop from './page/user/createShop/createShop'
+import "./App.css";
+import CreateShop from "./page/user/createShop/createShop";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ManagementProduct from "./page/shop/productManagement/ManagementProduct ";
+import Product from "./page/shop/productManagement/Product";
 
 function App() {
   return (
     <div>
-      <CreateShop/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/createShop" element={<CreateShop />}></Route>
+          <Route path="/admshop" element={<Product />}>
+            <Route path="products" element={<ManagementProduct />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/page/shop/componentDataTableAdmin/data.js
@@ -0,0 +1,8 @@
+const data = [
+   { img:'https://salt.tikicdn.com/cache/400x400/ts/product/77/e1/ea/36c51e524d468e3db84a916a474dd350.jpg.webp' , name: 'Quần', id: 1, price: 32000, address:100, contentScore: 'Xuất sắc' },
+   { img: 'https://salt.tikicdn.com/cache/400x400/ts/product/12/ee/89/f32398e34bd8d42d9ac845ecbd6a3c1b.png.webp' , name: 'Áo', id: 2, price: 32000, address:100, contentScore: 'Xuất sắc' },
+   { img:'https://salt.tikicdn.com/cache/400x400/ts/product/e3/27/cd/3385fecbeef634f350079caab9dacb91.jpg.webp' , name: 'Nước Hoa', id: 3, price: 32000, address:100, contentScore: 'Xuất sắc' },
+   { img:'https://salt.tikicdn.com/cache/400x400/ts/product/11/05/7e/7d3f154b703415d375c644d2cdf5f188.jpg.webp' , name: 'Mũ', id: 4, price: 32000, address:100, contentScore: 'Xuất sắc' },
+   { img:'https://cdn.vortexs.io/api/images/f382d6f0-42e4-4676-a7a9-ea4e256d37fd/1920/w/top-choice-giay-nike-air-force-1-low-all-white-w-315115-112.jpeg' , name: 'Giày', id: 5, price: 32000, address:100, contentScore: 'Xuất sắc' },
+]
+export default data
