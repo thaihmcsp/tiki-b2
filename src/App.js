70647c8014a76df97af3d5d5597df8f2c54f763b
Baocode0202 <devbaopham222000@gmail.com>
2022-07-29 17:31:51 +0700

    bao

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import "antd/dist/antd.css";
-import { Routes,BrowserRouter,Route } from "react-router-dom";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
-<<<<<<< HEAD
-import AddItem from "./page/shop/productManagement/priceItem/addItem_Phu/AddItem";
-import Product from "./page/shop/productManagement/Product";
-=======
 
 import ShopDashboard from "./page/shop/dashboard/ShopDashboard";
->>>>>>> b702c11123f77589944ba982ed3db1787d78d771
+import AddItem from "./page/shop/productManagement/priceItem/addItem_Phu/AddItem";
 function App() {
-  
   return (
     <div className="App">
-<<<<<<< HEAD
-      <BrowserRouter>
-        <Routes>
-            
-            <Route path="/addItem" element={<AddItem/>}/>
-            <Route index element={<Product />}/>
-        </Routes>
-      </BrowserRouter>
-=======
-      <ShopDashboard />
->>>>>>> b702c11123f77589944ba982ed3db1787d78d771
+      <AddItem />
     </div>
   );
 }
