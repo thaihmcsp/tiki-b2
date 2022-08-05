6fbcab66b0e4b2026be396cddec4c964535674cd
PhuDao998 <phudaohd98@gmail.com>
2022-08-05 10:33:42 +0700

    phu

--- src/page/shop/productManagement/priceItem/addItem_Phu/inforProduct/description/textArea/TextArea.js
+++ src/page/shop/productManagement/priceItem/addItem_Phu/inforProduct/description/textArea/TextArea.js
@@ -1,37 +1,44 @@
-import React, { Component, useRef } from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import React from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import './textArea.css'
 
-function TextArea({placeholder}) {
-    const editorRef = useRef(null);
-   const log = () => {
-     if (editorRef.current) {
-       console.log(editorRef.current.getContent());
-     }
-   };
-
+function TextArea({ handleChange, ...props }) {
+  function uploadAdapter(loader) {
+    return {
+      upload: () => {
+        return new Promise((resolve, reject) => {
+          const body = new FormData();
+          console.log('hello')
+          loader.file.then((file) => {
+            body.append("files", file);
+          });
+        });
+      }
+    };
+  }
+  function uploadPlugin(editor) {
+    editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+      return uploadAdapter(loader);
+    };
+  }
   return (
     <div>
        <div className="description">
-                <CKEditor 
-                    editor={ ClassicEditor }
-                    data="<p></p>"
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        // console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        // console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        // console.log( 'Focus.', editor );
-                    } }
-                />
+       <CKEditor
+        config={{
+          extraPlugins: [uploadPlugin]
+        }}
+        editor={ClassicEditor}
+        onReady={(editor) => {}}
+        onBlur={(event, editor) => {}}
+        onFocus={(event, editor) => {}}
+        onChange={(event, editor) => {
+          // handleChange(editor.getData());
+          console.log(56,editor)
+        }}
+        // {...props}
+      />
             </div>
     </div>
   )
