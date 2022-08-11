9590cf60c4ee75d4114e11c4d00515a6df0facdd
vangiap98 <nguyengiap4344@gmail.com>
2022-08-11 11:39:32 +0700

    recode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import ShopProfile from "./page/shop/shopProfile/ShopProfile";
 
@@ -7,5 +6,3 @@ function App() {
 }
 
 export default App;
-=======
->>>>>>> d22136af27879ef67dee30f6a2e57a7a2d26ff5f
--- src/page/shop/shopProfile/ProfileShop.jsx
+++ src/page/shop/shopProfile/ProfileShop.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ProfileShop(props) {
   return (
-    <div className="Footer-ProfileShop">
+    <div className="ProfileShop">
       <a href={props.href}>{props.title}</a>
       <i>{props.plus}</i>
       <h2>{props.decription}</h2>
